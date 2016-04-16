08ce70c03a5fc4d878cbae9291b0801e8d6cb5db
aspgirl <assspGirl@gmail.com>
2016-04-16 20:57:15 +0300

    first one

---
+++ yourS/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace yourS
+{
+    class Program
+    {
+        public static void Main()
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<ServiceController>(s =>
+                {
+                    s.ConstructUsing(name => new ServiceController());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription("MyFirstService");
+                x.SetDisplayName("MyFirstService");
+                x.SetServiceName("MyFirstService");
+            });
+        }
+    }
+}
---
+++ yourS/ServiceController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yourS
+{
+    public class ServiceController
+    {
+        private bool _isServiceAllowedToRun;
+
+        readonly Task _task;
+
+        public ServiceController()
+        {
+            _isServiceAllowedToRun = true;
+            _task = new Task(ServiceControllerRoutine);
+        }
+
+        private void ServiceControllerRoutine()
+        {
+            while (_isServiceAllowedToRun)
+            {
+                Console.WriteLine("Vanichka-Sone4ko");
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+        public void Start() { _task.Start(); }
+        public void Stop()
+        {
+            _isServiceAllowedToRun = false;
+            Console.WriteLine("Service was stopped");
+            Console.ReadLine();
+        }
+    }
+}
